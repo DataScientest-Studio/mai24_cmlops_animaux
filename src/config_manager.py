3818b6ff1926c67fb472292aae031a8072b62eea
Greg <greg_pechin@yahoo.fr>
2024-08-22 15:27:08 +0200

    Ajout fichier de paramètres de configuration

---
+++ src/config_manager.py
@@ -0,0 +1,22 @@
+# Définition des classes présentes
+CLASSES = ['Bear',
+           'Bird',
+           'Cat',
+           'Cow',
+           'Deer',
+           'Dog',
+           'Dolphin',
+           'Elephant',
+           'Giraffe',
+           'Horse',
+           'Kangaroo',
+           'Lion',
+           'Others',
+           'Panda',
+           'Tiger',
+           'Zebra']
+
+# Paramètres pour construction dataset
+TRAIN_WEIGHT = 0.8
+SEED = 255
+INTEGRATION_MIN = 10
